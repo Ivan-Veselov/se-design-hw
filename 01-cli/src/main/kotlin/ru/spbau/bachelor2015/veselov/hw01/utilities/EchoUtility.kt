d1256d1a2f855e8c19ba6588052db6e78c01b96d
Ivan Veselov <veselov.iva@gmail.com>
2018-02-26 19:13:21 +0300

    Add docs for echo and wc utilities.

--- 01-cli/src/main/kotlin/ru/spbau/bachelor2015/veselov/hw01/utilities/EchoUtility.kt
+++ 01-cli/src/main/kotlin/ru/spbau/bachelor2015/veselov/hw01/utilities/EchoUtility.kt
@@ -3,6 +3,9 @@ package ru.spbau.bachelor2015.veselov.hw01.utilities
 import ru.spbau.bachelor2015.veselov.hw01.ExecutionResult
 import ru.spbau.bachelor2015.veselov.hw01.environment.Utility
 
+/**
+ * Echo utility just return its arguments separated by space.
+ */
 object EchoUtility : Utility {
     override fun execute(args: List<String>, input: String): ExecutionResult {
         return ExecutionResult(
--- 01-cli/src/main/kotlin/ru/spbau/bachelor2015/veselov/hw01/utilities/WcUtility.kt
+++ 01-cli/src/main/kotlin/ru/spbau/bachelor2015/veselov/hw01/utilities/WcUtility.kt
@@ -7,6 +7,10 @@ import java.nio.charset.Charset
 import java.nio.file.Path
 import java.nio.file.Paths
 
+/**
+ * Wc utility counts number of newlines, words and bytes in given files. If command receives no
+ * arguments then input stream is used.
+ */
 object WcUtility : Utility {
     override fun execute(args: List<String>, input: String): ExecutionResult {
         val output = if (args.isEmpty()) {
